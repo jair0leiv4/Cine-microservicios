e8e0e22cbb755af1b3c691c1677156ca8df121b0
PN-Alumno <PN-Alumno@duoc.acad>
2026-05-16 10:25:36 -0400

    agrege al pom jakarta pero falta aun a los no hechos

--- cliente/src/main/java/cl/duoc/cliente/controller/ClienteController.java
+++ cliente/src/main/java/cl/duoc/cliente/controller/ClienteController.java
@@ -1,14 +1,21 @@
 package cl.duoc.cliente.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import cl.duoc.cliente.dto.ClienteRequestDTO;
 import cl.duoc.cliente.dto.ClienteResponseDTO;
 import cl.duoc.cliente.service.ClienteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
